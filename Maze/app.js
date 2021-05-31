596aef731582b637f012b40659eec0395b153739
nikitarougal <77426901+nikitarougal@users.noreply.github.com>
2021-05-31 09:32:51 +0300

    Update app.js

--- Maze/app.js
+++ Maze/app.js
@@ -13,19 +13,19 @@ function mazeSolving(maze) {
     let result = [];
 
     function checkPath(start, end) {
-        maze[start.y][start.x] = '0';
+        maze[start.y][start.x] = '0';//define each passed element as '0'
 
         let siblings = getValidSib(start);
-
+        //check if there any possible ways
         if (siblings.length > 0) {
 
             for (let i = 0; i < siblings.length; i++) {
                 let current = siblings[i];
-                let isSolved = current.x === end.x && current.y === end.y;
-                let notVisited = maze[current.y][current.x] !== '0';
-                result.push(current.direct);
+                let isSolved = current.x === end.x && current.y === end.y;//check if the possible way is the exit
+                let notVisited = maze[current.y][current.x] !== '0';//check if the possible way has been visited already
+                result.push(current.direct);//add the possible way to our resulting array
 
-                if (isSolved || (notVisited && checkPath(current, end))) {
+                if (isSolved || (notVisited && checkPath(current, end))) {//using recursion, invoke our function which goes through the whole route and return true
                     return true;
                 }
             }
@@ -37,7 +37,7 @@ function mazeSolving(maze) {
     function getValidSib(cord) {
         let { x, y } = cord;
         let cords = [];
-
+        //define all possible steps
         if (maze[y - 1] !== undefined) {
             cords.push({ x: x, y: y - 1, val: maze[y - 1][x], direct: 'top' });
         }
@@ -53,7 +53,7 @@ function mazeSolving(maze) {
         if (maze[y][x + 1] !== undefined) {
             cords.push({ x: x + 1, y: y, val: maze[y][x + 1], direct: 'right' })
         }
-
+        //choose only options marked as '+' (way)
         return cords.filter(el => el.val === '+');
     }
     checkPath({ x: 4, y: 3 }, { x: 0, y: 3 });
